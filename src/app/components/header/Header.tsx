2887ff6162ecf62162b0bbd04b7572ef61c04d86
Gor-Hoveyan <gorpro2004@mail.ru>
2024-04-08 02:31:59 +0400

    Navigation bug improved

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -8,11 +8,11 @@ export default function Header() {
         <header className={styles.header}>
             <nav>
                 <ul className={styles.headerContainer} >
-                    <li className={styles.headerLink}><h3><Link href={'http://localhost:3000/'}>Home</Link></h3></li>
-                    <li className={styles.headerLink}><h3><Link href={'http://localhost:3000/categories/men\'s clothing'}>Men's clothing</Link></h3></li>
-                    <li className={styles.headerLink}><h3><Link href={'http://localhost:3000/categories/women\'s clothing'}>Women's clothing</Link></h3></li>
-                    <li className={styles.headerLink}><h3><Link href={'http://localhost:3000/categories/electronics'}>Electronics</Link></h3></li>
-                    <li className={styles.headerLink}><h3><Link href={'http://localhost:3000/categories/jewelery'}>Jewelery</Link></h3></li>
+                    <li className={styles.headerLink}><h3><Link href={'/'}>Home</Link></h3></li>
+                    <li className={styles.headerLink}><h3><Link href={'/categories/men\'s clothing'}>Men's clothing</Link></h3></li>
+                    <li className={styles.headerLink}><h3><Link href={'/categories/women\'s clothing'}>Women's clothing</Link></h3></li>
+                    <li className={styles.headerLink}><h3><Link href={'/categories/electronics'}>Electronics</Link></h3></li>
+                    <li className={styles.headerLink}><h3><Link href={'/categories/jewelery'}>Jewelery</Link></h3></li>
                 </ul>
             </nav>
 
@@ -21,11 +21,11 @@ export default function Header() {
                 <label htmlFor="burger-toggle" className={styles.menuOpener}>&#9776;</label>
 
                 <ul className={styles.burgerMenu}>
-                    <li className={styles.menuLink}><h3><Link href={'http://localhost:3000/'}>Home</Link></h3></li>
-                    <li className={styles.menuLink}><h3><Link href={'http://localhost:3000/categories/men\'s clothing'}>Men's clothing</Link></h3></li>
-                    <li className={styles.menuLink}><h3><Link href={'http://localhost:3000/categories/women\'s clothing'}>Women's clothing</Link></h3></li>
-                    <li className={styles.menuLink}><h3><Link href={'http://localhost:3000/categories/electronics'}>Electronics</Link></h3></li>
-                    <li className={styles.menuLink}><h3><Link href={'http://localhost:3000/categories/jewelery'}>Jewelery</Link></h3></li>
+                    <li className={styles.menuLink}><h3><Link href={'/'}>Home</Link></h3></li>
+                    <li className={styles.menuLink}><h3><Link href={'/categories/men\'s clothing'}>Men's clothing</Link></h3></li>
+                    <li className={styles.menuLink}><h3><Link href={'/categories/women\'s clothing'}>Women's clothing</Link></h3></li>
+                    <li className={styles.menuLink}><h3><Link href={'/categories/electronics'}>Electronics</Link></h3></li>
+                    <li className={styles.menuLink}><h3><Link href={'/categories/jewelery'}>Jewelery</Link></h3></li>
                 </ul>
             </nav>
         </header>
